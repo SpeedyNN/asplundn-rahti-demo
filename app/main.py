d28638e0eda70ec603321f8d4b718929efd0b706
Nicke <87138573+SpeedyNN@users.noreply.github.com>
2026-04-01 10:07:21 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 
 app = FastAPI()
 
@@ -6,6 +6,10 @@ app = FastAPI()
 def read_root():
     return { "msg": "nå mårå nicke här", "v": "0.1" }
 
+@app.get("/api/ip")
+async def read_root(request: Request):
+    client_ip = request.client.host
+    return {"client_ip": client_ip}
 
 @app.get("/items/{id}")
 def read_item(item_id: int, q: str = None):
